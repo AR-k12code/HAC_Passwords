161c742ce66e8634a27459b1568f2f7198bab9ef
Craig Millsap <criag@carbm1.com>
2021-01-17 06:19:41 -0600

    CSV and some Error Control

--- README.md
+++ README.md
@@ -1,16 +1,13 @@
 # HAC_Passwords
+> node hacpassword.js -username 0401cmillsap -password [THIS SHOULD BE PULLED FROM YOUR ENCRYPTED PASSWORD] -studentid 40305966 -hacpassword "NewP@ssw0rd"
+> node hacpassword.js -username 0401cmillsap -password [THIS SHOULD BE PULLED FROM YOUR ENCRYPTED PASSWORD] -csv hac_passwords.csv
 
 ## Install needed modules:
 * npm i puppeteer
-* npm i dotenv
-
-## Create a .env file containing:
-* SSOUSERNAME='0401cmillsap'
-* PASSWORD='MYSUPERSECRETPASSWORDHERE'
-
+* npm i csv-parser
 
 ## Project Goals:
-* Lots of Error Control
-* Processing a CSV
-* Do not require password change
-* Set username to email or just username
+- [ ] Lots of Error Control
+- [x] Processing a CSV
+- [ ] Do not require password change
+- [ ] Set username to email or just username
--- hacpassword.js
+++ hacpassword.js
@@ -1,31 +1,50 @@
 const puppeteer = require('puppeteer');
-const dotenv = require('dotenv').config();
+//const dotenv = require('dotenv').config(); //Removed since we should call this from our PowerShell script using our encrypted password now.
+const csv = require('csv-parser');
+const fs = require('fs');
 
 // Expected command line should be in the following formats:
-// node hacpassword.js 40305966 NewP@ssw0rd
+// node hacpassword.js -username 0401cmillsap -password [THIS SHOULD BE PULLED FROM YOUR ENCRYPTED PASSWORD] -studentid 40305966 -hacpassword "NewP@ssw0rd"
+// node hacpassword.js -username 0401cmillsap -password [THIS SHOULD BE PULLED FROM YOUR ENCRYPTED PASSWORD] -csv hac_passwords.csv
 
-if (process.argv[2] === undefined) {
-    console.log('You must specify a StudentID or CSV to process.');
+
+if (process.argv.indexOf('-username') === -1) {
+  console.log("-username is a required parameter"); process.exit(1)
 } else {
-    if(process.argv[3] === undefined){
-        console.log('You must specify a password for StudentID',process.argv[2]);
-        process.exit(1)
-    }
+  var username = process.argv[process.argv.indexOf('-username') + 1]
+}
 
-    var StudentID = process.argv[2];
-    var HACPassword = process.argv[3];
+if (process.argv.indexOf('-password') === -1) {
+  console.log("-password is a required parameter"); process.exit(1)
+} else {
+  var password = process.argv[process.argv.indexOf('-password') + 1]
+}
 
-    console.log('Going to set student',StudentID,'with password of',HACPassword);
+//if csv is not specified then we use the supplied -studentid and -hacpassword.
+if (process.argv.indexOf('-csv') === -1) {
+  if (process.argv.indexOf('-studentid') === -1) {
+    console.log("-studentid is a required parameter"); process.exit(1)
+  } else {
+    var StudentID = process.argv[process.argv.indexOf('-studentid') + 1]
+  }
 
+  if (process.argv.indexOf('-hacpassword') === -1) {
+    console.log("-hacpassword is a required parameter"); process.exit(1)
+  } else {
+    var HACPassword = process.argv[process.argv.indexOf('-hacpassword') + 1]
+  }
+
+} else {
+  var csvPath = process.argv[process.argv.indexOf('-csv') + 1]
+  var studentsCSV = [];
 }
 
 // eSchool login
-
 (async () => {
-  //const browser = await puppeteer.launch();
   const browser = await puppeteer.launch({
     //headless: false
   });
+  
   const page = await browser.newPage();
 
   await page.setViewport({
@@ -34,41 +53,160 @@ if (process.argv[2] === undefined) {
     deviceScaleFactor: 1,
   });
 
-  await page.goto('https://eschool20.esp.k12.ar.us/eSchoolPLUS20/Account/LogOn?ReturnUrl=%2feSchoolPLUS20');
+  await page.setRequestInterception(true);
   
-  await page.type('#UserName', process.env.SSOUSERNAME);
-  await page.type('#Password', process.env.PASSWORD);
-  await page.click('#login');
-  await page.waitForNavigation({
-    waitUntil: 'networkidle0',
+  //Don't waste resources on proceessing CSS,Fonts, or Images. Continue with JavaScript.
+  page.on('request', (req) => {
+      if(req.resourceType() == 'stylesheet' || req.resourceType() == 'font' || req.resourceType() == 'image'){
+          req.abort();
+      }
+      else {
+          req.continue();
+      }
   });
-  
-  debugger;
-  await page.click('#setEnvOkButton');
 
-    // this doesn't always work.
-    //   await page.waitForNavigation({
-    //     waitUntil: 'networkidle0',
-    //   });
+  try {
+    await page.goto('https://eschool20.esp.k12.ar.us/eSchoolPLUS20/Account/LogOn?ReturnUrl=%2feSchoolPLUS20');
     
-  //We don't need the page to actually load so just navigate straight to the student after wait.
-  await page.waitForTimeout(1000);
+    await page.type('#UserName', username);
+    await page.type('#Password', password);
+    await page.click('#login');
+    await page.waitForNavigation({
+      waitUntil: 'networkidle0',
+    });
+    
+    // debugger;
+    await page.click('#setEnvOkButton');
+
+      // this doesn't always work.
+      //   await page.waitForNavigation({
+      //     waitUntil: 'networkidle0',
+      //   });
+      
+    //We don't need the page to actually load so just navigate straight to the student after wait.
+    await page.waitForTimeout(1000);
+    
+    // if csvPath is undefined then process the single student.
+    if (csvPath === undefined) {
+
+      console.log('Setting student',StudentID,'with password of',HACPassword);
+      //Go to student.
+      //await page.setDefaultNavigationTimeout(0); //this takes awhile
+      await page.goto('https://eschool20.esp.k12.ar.us/eSchoolPLUS20/Student/Registration/ContactDetail?studentId=' + StudentID + '&ContactPageMode=Address&PageEditMode=Modify&ContactEditMode=Modify')
+
+      //Must be typed into the box. Setting the value doesn't work.
+      await page.waitForSelector('#AddressOrContactDetail_PasswordToDisplay');
+      await page.focus('#AddressOrContactDetail_PasswordToDisplay')
+      await page.keyboard.type(HACPassword)
+
+      await page.click('#pageOptions-option-save');
+      await page.waitForTimeout(500);
+      
+      try {
+        //If the password passes the validation then we should see the confirmation button.
+        await page.waitForSelector('#saveWarning-yes', {
+          timeout: 1000
+        });
+        await page.click('#saveWarning-yes');
+        await page.waitForTimeout(1000);
+      } catch(err) {
+        //If the password failed validation then we need to log why.
+        await page.waitForSelector('#error-list > li');
+        let element = await page.$('#error-list > li');
+        let value = await page.evaluate(el => el.textContent, element);
+        console.log("Error:",value);
+      }
+
+      //without css you can't validate on visible.
+      //await page.waitForSelector('#EmailLoginPanel-content > div > div.alert.alert-danger.password-exists', { visible: true, });
+
+      await browser.close()
+      
+    } else {
+      //csv file has been supplied. Lets do some crazy looping.
+
+      fs.createReadStream(csvPath)
+      .pipe(csv())
+      //.on('data', async (row) => {
+      .on('data', async (row) => {
+        studentsCSV.push(row)
+      })
+      .on('end', async () => {
+        console.log('CSV file successfully processed');
+        for (let i = 0; i < studentsCSV.length; i++) {
+          //studentsCSV[i]
+            var StudentID = studentsCSV[i].Student_id
+            var HACPassword = studentsCSV[i].Password
+            //console.log(row);
   
-  //Go to student.
-  //await page.setDefaultNavigationTimeout(0); //this takes awhile
-  await page.goto('https://eschool20.esp.k12.ar.us/eSchoolPLUS20/Student/Registration/ContactDetail?studentId=' + StudentID + '&ContactPageMode=Address&PageEditMode=Modify&ContactEditMode=Modify')
+            console.log('Going to set student',StudentID,'with password of',HACPassword);
+            try {
+              //Go to student.
+              //await page.setDefaultNavigationTimeout(0); //this takes awhile
 
-  //Must be typed into the box. Setting the value doesn't work.
-  await page.waitForSelector('#AddressOrContactDetail_PasswordToDisplay');
-  await page.focus('#AddressOrContactDetail_PasswordToDisplay')
-  await page.keyboard.type(HACPassword)
+              let stuPage = await browser.newPage();
 
-  await page.click('#pageOptions-option-save');
-  await page.waitForTimeout(1000);
-  await page.waitForSelector('#saveWarning-yes');
-  await page.click('#saveWarning-yes');
-  await page.waitForTimeout(1000);
+              await stuPage.setViewport({
+                width: 1366,
+                height: 768,
+                deviceScaleFactor: 1,
+              });
 
-  await browser.close()
+              await stuPage.setRequestInterception(true);
+              
+              //Don't waste resources on proceessing CSS,Fonts, or Images. Continue with JavaScript.
+              await stuPage.on('request', (req) => {
+                  if(req.resourceType() == 'stylesheet' || req.resourceType() == 'font' || req.resourceType() == 'image'){
+                      req.abort();
+                  }
+                  else {
+                      req.continue();
+                  }
+              });
+              await stuPage.goto('https://eschool20.esp.k12.ar.us/eSchoolPLUS20/Student/Registration/ContactDetail?studentId=' + StudentID + '&ContactPageMode=Address&PageEditMode=Modify&ContactEditMode=Modify')
+  
+              //Must be typed into the box. Setting the value doesn't work.
+              await stuPage.waitForSelector('#AddressOrContactDetail_PasswordToDisplay');
+              await stuPage.focus('#AddressOrContactDetail_PasswordToDisplay')
+              await stuPage.keyboard.type(HACPassword)
+  
+              await stuPage.click('#pageOptions-option-save');
+              await stuPage.waitForTimeout(1000);
+
+              try {
+                //If the password passes the validation then we should see the confirmation button.
+                await stuPage.waitForSelector('#saveWarning-yes', {
+                  timeout: 1000
+                });
+                await stuPage.click('#saveWarning-yes');
+                await stuPage.waitForTimeout(1000);
+              } catch(err) {
+                //If the password failed validation then we need to log why.
+                await stuPage.waitForSelector('#error-list > li');
+                let element = await stuPage.$('#error-list > li');
+                let value = await stuPage.evaluate(el => el.textContent, element);
+                console.log("Error:",value);
+              }
+
+              await stuPage.close();
+
+            } catch(err) {
+              console.log("Failed to update",StudentID,": ",err)
+            }
+
+            //if we reached the end of the loop then close completely.
+            if (i === studentsCSV.length - 1) {
+              await browser.close()
+            }
+
+        }
+      });
+
+    }
+
+  } catch(err) {
+    console.log('Failed to update password.')
+    process.exit(1)
+  }
 
 })();
